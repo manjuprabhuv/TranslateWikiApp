4a787a90c5ded8122d945e9ca94744129e5b9072
Manjunath V Prabhu <manjuprabhuv@gmail.com>
2012-11-11 00:01:36 +0530

    On Language selection, Navigate to Translation page

--- src/net/translatewiki/PreferenceActivity.java
+++ src/net/translatewiki/PreferenceActivity.java
@@ -1,13 +1,38 @@
 package net.translatewiki;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.preference.EditTextPreference;
+import android.preference.Preference;
+import android.preference.PreferenceManager;
 
 import com.actionbarsherlock.app.SherlockPreferenceActivity;
 
 public class PreferenceActivity extends SherlockPreferenceActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.preferences);
+		EditTextPreference languagePreference = (EditTextPreference) findPreference("language");
+		final String sLang = PreferenceManager
+				.getDefaultSharedPreferences(this).getString("language", "en");
+		languagePreference
+				.setOnPreferenceChangeListener(new Preference.OnPreferenceChangeListener() {
+					@Override
+					public boolean onPreferenceChange(Preference preference,
+							Object lang) {
+						String sNewlang = (String) lang;
+						if (!sNewlang.equalsIgnoreCase(sLang)) {
+							Intent intent = new Intent(getApplicationContext(),
+									ProofReadActivity.class);
+							startActivity(intent);
+						}else{
+						PreferenceActivity.this.finish();
+						}
+						return true;
+					}
+				});
+
+	}
+
 }
